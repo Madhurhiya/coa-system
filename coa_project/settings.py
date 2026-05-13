f664a81644ce2f78e90d1681c6d35366795572e4
Madhurhiya <madhur@hiyaindia.in>
2026-05-13 16:36:18 +0530

    Fix static files and whitenoise

--- coa_project/settings.py
+++ coa_project/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 # Database — add this block at the bottom
